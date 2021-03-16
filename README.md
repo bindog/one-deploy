a1e181b33dd274238eb272893e1aa7d2adfadb0a
bindog <bindog@outlook.com>
2021-03-16 11:24:19 +0800

    add cortex predictors & dependencies

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# one-deploy
+# 一步 one-deploy
+
+## 简介
+
+深度学习自动部署框架，整合了多个优秀开源项目，让部署变成简易轻松
+
+## 特性
+
+- 支持多种模型推理(PyTorch/ONNX/TF)
+- API服务情况监控
+- 微服务架构/容器化部署
+- 服务自动注册/发现
+- 反向代理/动态负载均衡
+
+## 短期计划
+
+- nginx零重启更新upstream
+- 自动弹性扩展
+- 增加压测脚本
+- 增加支持其他类型反向代理和负载均衡工具，如[traefik](https://github.com/traefik/traefik/)
+- 增加CV方向的Demo和测试模型
+- 文档说明
+
+## 长期计划
+
+- 支持本地k8s集群
+- 模型修改工具
+- 网页前端进行配置管理
+
+## 整合开源项目列表
+
+- 官方[cortex](https://github.com/cortexlabs/cortex), 魔改[cortex](https://github.com/bindog/cortex)
+- 官方[registrator](https://github.com/gliderlabs/registrator), 魔改[registrator]()
+- [consul](https://github.com/hashicorp/consul)
+- [consul-template](https://github.com/hashicorp/consul-template)
---
+++ predictors/predictor_seg.py
@@ -0,0 +1,23 @@
+import sys
+from LAC import LAC
+from stopwordsiso import stopwords
+
+
+class PythonPredictor:
+    def __init__(self, config):
+        self.all_stopwords = stopwords(["en", "zh"])
+        use_cuda_flag = config.get("use_cuda", False)
+        self.model = LAC(mode='seg', use_cuda=use_cuda_flag)
+
+    def predict(self, query_params, payload):
+        input_list = [p["text"] for p in payload]
+        result_list = self.model.run(input_list)
+        final_result = []
+        for i, result in enumerate(result_list):
+            clean_flag = query_params[i].get("clean", False)
+            if clean_flag:
+                clean_result = [w for w in result if w not in self.all_stopwords]
+                final_result.append(clean_result)
+            else:
+                final_result.append(result)
+        return final_result
